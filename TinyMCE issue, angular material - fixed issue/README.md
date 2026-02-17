573f445c492791ccd759e99582a2526ead10ec1c
michalnieruchalski-tiugo <michal.nieruchalski@tiny.cloud>
2026-02-17 21:51:11 +0100

    init

---
+++ TinyMCE issue, angular material - fixed issue/README.md
@@ -0,0 +1,11 @@
+# Summary
+This repository demonstrates a potential fix to the problem where TinyMCE becomes inoperable when the editor component is rendered inside material tab.
+The idea is to use `preserveContent` property.
+See `src/main.ts` file
+
+# Startup
+```
+npm install
+npm start
+```
+
---
+++ TinyMCE issue, angular material - fixed issue/src/main.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { EditorComponent, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
+import { MatTabsModule } from '@angular/material/tabs';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+@Component({
+  selector: 'app-root',
+  imports: [EditorComponent, MatTabsModule],
+  providers: [
+    { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' },
+  ],
+  template: `
+    <mat-tab-group preserveContent>
+      <mat-tab label="Header">
+        <h1>Header</h1>
+        <editor />
+      </mat-tab>
+      <mat-tab label="Body">
+        <h1>Body</h1>
+        <editor />
+      </mat-tab>
+      <mat-tab label="Footer">
+        <h1>Footer</h1>
+        <editor />
+      </mat-tab>
+    </mat-tab-group>
+  `,
+})
+export class App {}
+
+bootstrapApplication(App, {
+  providers: [provideAnimations()],
+});
